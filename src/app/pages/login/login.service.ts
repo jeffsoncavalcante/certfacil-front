a67827a0812cdbca27bf9401b88e3b2d29dfb63d
unknown <vitor.cavalcante@redetuxnet.com.br>
2021-04-25 21:35:11 -0300

    feature-login branch login

---
+++ src/app/pages/login/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, retry, take } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoginService {
+  private readonly API = 'http://186.195.8.9:8000/api/user/login'
+  constructor(private http:HttpClient) { }
+
+  create(login){
+    return this.http.post(this.API,login).pipe(take(1));
+  }
+}
