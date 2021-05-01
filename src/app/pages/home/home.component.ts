2087dfabf47accbfefe75145af73037c98e29c35
unknown <vitor.cavalcante@redetuxnet.com.br>
2021-04-30 23:56:33 -0300

    Feature: Permicoes de no home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CreateeventComponent } from './../createevent/createevent.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  typeuser = window.localStorage.getItem('usertype')
+  profile = false
+  createevent = false
+  home = false
+  myevents = false
+  event = false
+  certificate =false
+  listpresent= false
 
   constructor() { }
 
   ngOnInit(): void {
+    if(this.typeuser==="master"){
+      this.home=true,
+      this.createevent=true,
+      this.listpresent=true,
+      this.profile=true
+    }
+    if(this.typeuser==="palestrante"){
+      this.home=true,
+      this.certificate=true,
+      this.profile=true
+    }
+    if(this.typeuser==="participante"){
+      this.home=true,
+      this.event=true,
+      this.myevents=true,
+      this.profile=true
+    }
   }
 
 }
--- src/app/pages/login/login.service.ts
+++ src/app/pages/login/login.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { AlertTypes } from './../../shared/alert-modal.service';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, retry, take } from 'rxjs/operators';
 
@@ -9,10 +10,11 @@ import { catchError, retry, take } from 'rxjs/operators';
 export class LoginService {
   private readonly API = 'http://certapi.redetuxnet.com.br:8000'
   constructor(private http:HttpClient) {}
-
+  usertype = new EventEmitter<boolean>()
 
   create(login){
     return this.http.post(this.API+'/api/user/login',login).pipe(take(1));
+
   }
 
 }
