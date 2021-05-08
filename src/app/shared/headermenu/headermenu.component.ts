75d3b43bc21f244039c50608b6c3fddf51ec790f
Beatriz Alves <beatrizalvesfel@gmail.com>
2021-05-07 21:29:57 -0300

    Feature: pagina eventos

--- src/app/shared/headermenu/headermenu.component.ts
+++ src/app/shared/headermenu/headermenu.component.ts
@@ -5,58 +5,51 @@ import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-headermenu',
   templateUrl: './headermenu.component.html',
-  styleUrls: ['./headermenu.component.scss']
+  styleUrls: ['./headermenu.component.scss'],
 })
 export class HeadermenuComponent implements OnInit {
-
-  typeuser = window.localStorage.getItem('usertype')
-  token = window.localStorage.getItem('token')
-  profile = false
-  createevent = false
-  home = false
-  myevents = false
-  event = false
-  certificate =false
-  listpresent= false
+  typeuser = window.localStorage.getItem('usertype');
+  token = window.localStorage.getItem('token');
+  profile = false;
+  createevent = false;
+  home = false;
+  myevents = false;
+  event = false;
+  certificate = false;
+  listpresent = false;
   form: any;
 
-
-  constructor(private service: HeadermenuService, private fb: FormBuilder) { }
-
+  constructor(private service: HeadermenuService, private fb: FormBuilder) {}
 
   ngOnInit() {
-
     this.form = this.fb.group({
-      id_token: this.token
-    })
+      id_token: this.token,
+    });
 
-    if(this.typeuser==="master"){
-      this.home=true,
-      this.createevent=true,
-      this.listpresent=true,
-      this.profile=true
+    if (this.typeuser === 'master') {
+      (this.home = true),
+        (this.createevent = true),
+        (this.listpresent = true),
+        (this.profile = true);
     }
-    if(this.typeuser==="palestrante"){
-      this.home=true,
-      this.certificate=true,
-      this.profile=true
+    if (this.typeuser === 'palestrante') {
+      (this.home = true), (this.certificate = true), (this.profile = true);
     }
-    if(this.typeuser==="participante"){
-      this.home=true,
-      this.event=true,
-      this.myevents=true,
-      this.profile=true
+    if (this.typeuser === 'participante') {
+      (this.home = true),
+        (this.event = true),
+        (this.myevents = true),
+        (this.profile = true);
     }
   }
 
-  logout(){
+  logout() {
     this.service.logout(this.form.value).subscribe(
-      data => {
-        window.location.href="/index"
-        window.localStorage.clear()
+      (data) => {
+        window.location.href = '/index';
+        window.localStorage.clear();
       },
-      error => console.log(error)
-    )
+      (error) => console.log(error)
+    );
   }
-
 }
