5d0273353a7aa98d7e112f64f5edf8e44df18ca6
Beatriz Alves <beatrizalvesfel@gmail.com>
2021-05-12 23:07:02 -0300

    Feature: layout certificado

--- src/app/pages/pdf/pdf.component.ts
+++ src/app/pages/pdf/pdf.component.ts
@@ -1,26 +1,20 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { jsPDF } from "jspdf";
-
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-pdf',
   templateUrl: './pdf.component.html',
-  styleUrls: ['./pdf.component.css']
+  styleUrls: ['./pdf.component.css'],
 })
-export class PdfComponent  {
+export class PdfComponent {
+  @ViewChild('content', { static: false }) el!: ElementRef;
 
-  @ViewChild('content', {static:false}) el!: ElementRef;
-
- makePDF(){
-
-  let  pdf= new jsPDF ( 'p', 'pt', 'a4');
-  pdf.html(this.el.nativeElement,{
-    callback: (pdf)=>{
-      pdf.save("certificado.pdf");
-    }
-  });
- 
-
- }
-
+  makePDF() {
+    let pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save('certificado.pdf');
+      },
+    });
+  }
 }
